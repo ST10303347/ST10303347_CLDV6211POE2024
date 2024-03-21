242cadf8facd29d2386853e7c3147baa2ef5aaeb
ZAYYYY <ZAYYYY@ZAYYY999>
2024-03-21 17:33:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10303347_CLDV6211POE2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

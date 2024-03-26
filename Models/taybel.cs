c5bbb3d91d79d94c3e15f833c1654fb8c8b0e6fd
ZAYYYY <st10303347@vcconnect.edu.za>
2024-03-26 10:27:35 +0200

    before this added new layout theme. now added the table and sql Azure connection

---
+++ Models/taybel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ST10303347_CLDV6211POE2024.Models
+{
+    public class taybel : Controller
+    {
+
+        public static string con_string = "Server=tcp:zayyyserver.database.windows.net,1433;Initial Catalog=ZAYYYDATA;Persist Security Info=False;User ID=loudeyes;Password=juiceWRLD999;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection(con_string);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
